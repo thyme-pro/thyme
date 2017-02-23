c3a798a9659dc0c988da4c1ce4527e75ca961966
Thomas Thune Hansen <thomas.thune@vaimo.com>
2017-02-23 11:15:57 +0100

    Handle dev mode with environment flag THYME_ENABLE_DEV

--- config.js
+++ config.js
@@ -3,13 +3,19 @@ const path = require('path');
 let APP_NAME = 'Thyme';
 let APP_VERSION = require('./package.json').version;
 
+let IS_DEV = false;
+
+if (process.env.THYME_ENABLE_DEV) {
+  IS_DEV = (process.env.THYME_ENABLE_DEV == 'true') ? true : false;
+}
+
 module.exports = {
   APP_NAME: APP_NAME,
   APP_VERSION: APP_VERSION,
 
   ROOT_DIR: __dirname,
 
-  IS_DEV: false,
+  IS_DEV: IS_DEV,
 
   HOME_PAGE_URL: 'http://thyme.pro',
   GITHUB_URL: 'https://github.com/thyme-pro/thyme',
